f3785ad1e2ed8459b00624f453a29d720360ed65
aleksandraag <ola.golinska@vp.pl>
2023-03-26 00:07:39 +0100

    js

---
+++ main.js
@@ -0,0 +1,51 @@
+const currentNumber = document.querySelector(".currentNumber");
+
+const previousNumber = document.querySelector(".previousNumber p");
+
+const mathSign = document.querySelector(".mathSign");
+
+const numbersButtons = document.querySelectorAll(".number");
+
+const operatorsButtons = document.querySelectorAll(".operator");
+
+const equalsButton = document.querySelector(".equals");
+
+const clearButton = document.querySelector(".clear");
+
+const calculatorHistory = document.querySelector(".history");
+
+const historyBtn = document.querySelector(".history-btn");
+
+let result = " ";
+
+// Funkcje
+
+function displayNumbers() {
+  if (this.textContent === "." && currentNumber.innerHTML.includes(".")) return;
+  if (this.textContent === "." && currentNumber.innerHTML === "")
+    return (currentNumber.innerHTML = ".0");
+
+  currentNumber.innerHTML += this.textContent;
+}
+
+function operate() {}
+
+function showResult() {}
+
+function clearScreen() {}
+
+function clearHistory() {}
+
+// Nasłuchiwanie buttonów
+
+operatorsButtons.forEach((button) => button.addEventListener("click", operate));
+
+equalsButton.addEventListener("click", showResult);
+
+clearButton.addEventListener("click", clearScreen);
+
+numbersButtons.forEach((button) => {
+  button.addEventListener("click", displayNumbers);
+});
+
+historyBtn.addEventListener("click", clearHistory);
